35ef5c453911c70d0413fb772c87634497d13b6e
Richard Kopelow <richardkopelow@gmail.com>
2018-07-28 13:24:20 -0400

    SO variables can save in play mode or not

--- Variables/VariableTypes/BaseVariable.cs
+++ Variables/VariableTypes/BaseVariable.cs
@@ -6,11 +6,52 @@ namespace ScriptableObjectFramework
 {
     public abstract class BaseVariable<T> : ScriptableObject
     {
-        public T Value;
+        [SerializeField]
+        private bool saveInPlayMode;
+        public bool SaveInPlayMode
+        {
+            get
+            {
+                return saveInPlayMode;
+            }
+            set
+            {
+                saveInPlayMode = value;
+                if (saveInPlayMode)
+                {
+                    this.value = currentValue;
+                }
+            }
+        }
+
+        private T currentValue;
+
+        [SerializeField]
+        private T value;
+
+        public T Value
+        {
+            get
+            {
+                return saveInPlayMode ? value : currentValue;
+            }
+            set{
+                currentValue = value;
+                if (saveInPlayMode)
+                {
+                    this.value = value;
+                }
+            }
+        }
+
+        protected void OnEnable()
+        {
+            currentValue = value;
+        }
 
         public static implicit operator T(BaseVariable<T> variable)
         {
             return variable.Value;
         }
-    } 
+    }
 }
