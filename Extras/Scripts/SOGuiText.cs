2244b9978dde7bfc858c4ff74d49cb908d523464
Richard Kopelow <richardkopelow@gmail.com>
2018-08-18 09:38:03 -0400

    Restructured files and created SOGuiText

---
+++ Extras/Scripts/SOGuiText.cs
@@ -0,0 +1,25 @@
+﻿using ScriptableObjectFramework;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SOGuiText : MonoBehaviour
+{
+    public TextMeshProUGUI Target;
+    public BaseVariable Value;
+    public StringValue Format;
+
+    void Start()
+    {
+        if (Target == null)
+        {
+            Target = GetComponent<TextMeshProUGUI>();
+        }
+    }
+
+    void Update()
+    {
+        Target.text = Format == null ? Value.GetValue().ToString() : string.Format(Format, Value.GetValue());
+    }
+}
