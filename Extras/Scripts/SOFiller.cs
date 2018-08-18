d6fbf68db23995a066f26234f919722c5ae559ea
Richard Kopelow <richardkopelow@gmail.com>
2018-08-18 11:46:29 -0400

    Added SObased fill for Floats, and made a slider prefab

---
+++ Extras/Scripts/SOFiller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SOFiller : MonoBehaviour
+{
+    public Image Target;
+    public FloatValue Value;
+    public FloatValue Max;
+    public FloatValue Min;
+
+    void Start()
+    {
+        if (Target == null)
+        {
+            Target = GetComponent<Image>();
+        }
+    }
+    
+    void Update()
+    {
+        Target.fillAmount = (Value - Min) / (Max - Min);
+    }
+}
