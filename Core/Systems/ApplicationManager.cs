946e77ec6c000c2d8773b7e23ea26685840372c7
Richard Kopelow <richardkopelow@gmail.com>
2018-09-30 18:43:40 -0400

    Started ApplicationManager

---
+++ Core/Systems/ApplicationManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScriptableObjectFramework.Systems.ApplicationManager
+{
+    [CreateAssetMenu(fileName = "NewApplicationManager", menuName = "Scriptable Objects/Systems/ApplicationManager")]
+    public class ApplicationManager : ScriptableObject
+    {
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    } 
+}
