91da3215436b8287ba5c311ceffd9bee2d9d7f12
Richard Kopelow <richardkopelow@gmail.com>
2018-11-03 20:51:39 -0400

    Application manager now ends play mode in editor on quit

--- Core/Systems/ApplicationManager/ApplicationManager.cs
+++ Core/Systems/ApplicationManager/ApplicationManager.cs
@@ -7,9 +7,25 @@ namespace ScriptableObjectFramework.Systems.ApplicationManager
     [CreateAssetMenu(fileName = "NewApplicationManager", menuName = "Scriptable Objects/Systems/ApplicationManager")]
     public class ApplicationManager : ScriptableObject
     {
+        public float TimeScale
+        {
+            get
+            {
+                return Time.timeScale;
+            }
+            set
+            {
+                Time.timeScale = value;
+            }
+        }
+
         public void Quit()
         {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
             Application.Quit();
+#endif
         }
-    } 
+    }
 }
